0276cdc74853c3ad0ef30607afa7b60638add93b
lybroman <lybroman@hotmail.com>
2019-01-28 09:26:51 +0800

    Regular Checkin
    Signed-off-by: lybroman <lybroman@hotmail.com>

--- basics/recursion.py
+++ basics/recursion.py
@@ -33,18 +33,69 @@ def fb(recur, n):
 	return recur(n - 1) + recur(n - 2)
 
 
+# cache
+f = {0: 1, 1: 1}
+
+for i in xrange(2, 11):
+	f[i] = f[i - 1] + f[i - 2]
+print 'f(10):',  f[10]
+
+# general
+a, b = 1, 1
+for i in xrange(2, 11):
+	c = a + b
+	a, b = b, c
+
+print 'f(10):',  c
+
 fb = memoizer({0: 0, 1: 1}, fb)
 
 for i in xrange(10):
 	print fb(i)
 
 
-def fac(recur, n):
+def factorial(recur, n):
 	return n * recur(n - 1)
 
 
 memo = {1: 1}
-fac = memoizer(memo, fac)
+factorial = memoizer(memo, factorial)
 
 for i in xrange(1, 5):
-	print fac(i), memo
+	print factorial(i), memo
+
+# memo decorator
+# python3 lru_cache
+from functools import wraps
+
+
+def memo(func):
+	cache = {}
+
+	@wraps(func)
+	def wrapper(*args):
+		if args in cache:
+			return cache[args]
+		cache[args] = func(*args)
+		return cache[args]
+
+	return wrapper
+
+
+# binomial coefficient
+# pascal triangle
+# path count
+n, k = 10, 7
+row = {}
+for i in xrange(n + 1):
+	row[(i, 0)], row[(i, i)] = 1, 1
+	if i == 0 or i == 1:
+		continue
+	for j in xrange(1, i):
+		row[(i, j)] = row[(i - 1, j - 1)] + row[(i - 1, j)]
+
+print row[(10, 7)]
+
+
+
+
